9954009ab88f564dc0f6a8934ecceb926900252b
Cloves Neto <cvr.neto20@gmail.com>
2025-09-25 18:15:04 -0300

    feat: ajuste de responsividade dos botões
    Ajustei o tamanho  as classes de cores do botão além de trocar o componente de renderização para a compilação do código diminuir o tempo de carregamento com a própria tag do Image do next.

--- src/app/components/AnimatedText.tsx
+++ src/app/components/AnimatedText.tsx
@@ -10,40 +10,44 @@ interface AnimatedTextProps {
 
 export default function AnimatedText({ text }: AnimatedTextProps) {
   const words = text.split(' ');
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: (i = 1) => ({
-      opacity: 1,
-      transition: { staggerChildren: 0.1, delayChildren: i * 0.1 },
-    }),
-  };
 
-  const itemVariants = {
-    hidden: { y: 20, opacity: 0 },
-    visible: { y: 0, opacity: 1 },
-  };
+  const baseDelay = 0.08;
+  let totalCharIndex = 0;
 
   return (
+
     <motion.div
-      variants={containerVariants}
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
       className="flex justify-center items-center flex-wrap gap-x-2"
     >
-      {words.map((word, index) => (
-        <span key={index} className="inline-block"> 
-          {Array.from(word).map((char, charIndex) => (
-            <motion.span
-              key={charIndex}
-              variants={itemVariants}
-              className="inline-block"
-            >
-              {char === ' ' ? '\u00A0' : char}
-            </motion.span>
-          ))}
+      {words.map((word, wordIndex) => (
+        <span key={wordIndex} className="inline-block whitespace-nowrap">
+          {Array.from(word).map((char, charIndex) => {
+            const currentDelay = totalCharIndex * baseDelay;
+            totalCharIndex++;
+
+            return (
+              <motion.span
+                key={charIndex}
+                animate={{
+                  y: ['0%', '-30%', '0%'],
+                  transition: {
+                    duration: 1.8,
+                    ease: 'easeInOut',
+                    repeat: Infinity,
+                    repeatType: 'loop' as const,
+                    delay: currentDelay,
+                  },
+                }}
+                className="inline-block text-4xl font-extrabold"
+              >
+                {char === ' ' ? '\u00A0' : char}
+              </motion.span>
+            );
+          })}
+          {wordIndex < words.length - 1 && <span className="inline-block">&nbsp;</span>}
         </span>
-      ))}
-    </motion.div>
+      ))
+      }
+    </motion.div >
   );
 }
--- src/app/pato/page.tsx
+++ src/app/pato/page.tsx
@@ -1,41 +1,68 @@
 'use client';
 
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from '@/app/home.module.css';
-import {Icon} from '@iconify/react';
+import { Icon } from '@iconify/react';
+import Image from 'next/image';
 
 
 export default function MobileApp() {
 
+  const width = '30';
+  const height = '30';
+  const colors = {
+    pink: 'bg-[#FAC7CA]',
+    green: 'bg-[#C2D191]',
+    yellow: 'bg-[#FFFEAF]',
+    purple: 'bg-[#e0adeb]'
+  }
+
+
   return (
     <main className={styles.mainContainer}>
-      <a href='/' className='w-8 h-8 absolute top-0 left-0 text-6xl z-10 text-[#C2D191]' >
+      <Link href='/' className='w-8 h-8 absolute top-0 left-0 text-6xl z-10 text-[#C2D191]' >
         <Icon icon={'icon-park-solid:back'} />
-      </a >
+      </Link >
 
-      <img alt='pato caminhando no convite' src={'/images/pato.jpg'} className='w-full h-screen absolute top-0 left-0 object-fill' />
+      <Image width={1200} height={720} alt='pato caminhando no convite' src={'/images/pato.jpg'} className='w-screen h-screen' />
 
-      <div className="absolute w-72 h-auto z-10 flex flex-col gap-5 bottom-10 left-1/2 -translate-1/2">
-        <Link href="/convite" className="bg-[#FAC7CA] flex items-center justify-evenly gap-4 overflow-hidden max-h-20 text-black text-shadow-amber-50 text-shadow-2xs text-center shadow-[0_10px_8px_0px_rgba(0,0,0,0.5)] text-4xl px-8 rounded-full">
-          <img src={"/images/letter.png"} alt="letter icon" className='w-20 h-40 pt-2 object-cover' />
-          Convite
+      <div className="w-full h-screen z-10 absolute top-0 left-0 flex flex-col justify-end py-40 items-center">
+        <Link href="/convite" className={`${colors.pink} scale-75 flex flex-row w-full max-w-72 h-20 overflow-clip px-2 rounded-full shadow-[0_8px_8px_rgba(0,0,0,0.4)] `}>
+          <div className='w-1/3 h-full flex items-center justify-center pt-2'>
+            <Image width={width} height={height} src={"/images/letter.png"} alt="letter icon" className='w-20' />
+          </div>
+          <div className='w-2/3 h-full flex items-center justify-start text-4xl text-black text-shadow-2xs text-shadow-amber-50'>
+            Convite
+          </div>
         </Link>
 
-        <Link href="/cardapio" className="bg-[#FFFEAF] flex items-center justify-evenly gap-4 overflow-hidden max-h-20 text-black text-shadow-amber-50 text-shadow-2xs text-center shadow-[0_10px_8px_0px_rgba(0,0,0,0.5)] text-4xl px-8 rounded-full">
-          <img src={"/images/cake.png"} alt="letter icon" className='w-20 h-40 pt-2 object-cover' />
-          Cardápio
+        <Link href="/convite" className={`${colors.yellow} scale-75 flex flex-row w-full max-w-72 h-20 overflow-clip px-2 rounded-full shadow-[0_8px_8px_rgba(0,0,0,0.4)] `}>
+          <div className='w-1/3 h-full flex items-center justify-center pt-2'>
+            <Image width={width} height={height} src={"/images/cake.png"} alt="letter icon" className='w-20' />
+          </div>
+          <div className='w-2/3 h-full flex items-center justify-start text-4xl text-black text-shadow-2xs text-shadow-amber-50'>
+            Cardápio
+          </div>
         </Link>
 
-        <Link href="/dresscode" className="bg-[#C2D191] flex items-center justify-evenly gap-4 overflow-hidden max-h-20 text-black text-shadow-amber-50 text-shadow-2xs text-center shadow-[0_10px_8px_0px_rgba(0,0,0,0.5)] text-4xl px-4 rounded-full">
-          <img src={"/images/dress.png"} alt="letter icon" className='w-20 h-40 pt-2 object-cover' />
-          Dress Code
+        <Link href="/convite" className={`${colors.green} scale-75 flex flex-row w-full max-w-72 h-20 overflow-clip px-2 rounded-full shadow-[0_8px_8px_rgba(0,0,0,0.4)] `}>
+          <div className='w-1/3 h-full flex items-center scale-75 justify-center pt-2'>
+            <Image width={width} height={height} src={"/images/dress.png"} alt="letter icon" className='w-20' />
+          </div>
+          <div className='w-2/3 h-full flex items-center justify-start text-4xl text-black text-shadow-2xs text-shadow-amber-50'>
+            Dress Code
+          </div>
         </Link>
 
-        <Link href="/games" className="bg-[#e0adeb] flex items-center justify-evenly gap-4 overflow-hidden max-h-20 text-black text-shadow-amber-50 text-shadow-2xs text-center shadow-[0_10px_8px_0px_rgba(0,0,0,0.5)] text-4xl px-8 rounded-full">
-          <img src={"/images/letter.png"} alt="letter icon" className='w-20 h-40 pt-2 object-cover' />
-          Games
+        <Link href="/convite" className={`${colors.purple} scale-75 flex flex-row w-full max-w-72 h-20 overflow-clip px-2 rounded-full shadow-[0_8px_8px_rgba(0,0,0,0.4)] `}>
+          <div className='w-1/3 h-full flex items-center justify-center pt-2'>
+            <Image width={width} height={height} src={"/images/cake.png"} alt="letter icon" className='w-20' />
+          </div>
+          <div className='w-2/3 h-full flex items-center justify-start text-4xl text-black text-shadow-2xs text-shadow-amber-50'>
+            Games
+          </div>
         </Link>
+
       </div>
     </main>
   );
